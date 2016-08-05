610b1d3549d9ef9fcea06e06072d9a26b94be4c5
rahul.bu <rahul.bu@DV-MAC-RAHUL-BU.local>
2016-08-05 15:14:48 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import Board from './Board';
+import { observe } from './Game';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      knightPosition: [0,0]
+    }
+  }
+
+  componentDidMount = () => {
+    observe(this.observer);
+  }
+
+  observer = (knightPosition) => {
+    this.setState({knightPosition});
+  }
+
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <Board knightPosition={this.state.knightPosition} />
+      </div>
     );
   }
 }
